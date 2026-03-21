3306605cce1fab1fb865b747706cc8d1618abce1
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-03-21 19:51:03 +0600

    day1

--- day01.js
+++ day01.js
@@ -2,28 +2,37 @@
 
 let a = 4
 let result = (a > 0) ? "positive" : "negative"
-// console.log(result)
 
 // task 2 : age checker 
 
 let age = 22
 let validity = (age >= 18) ? "adult" : "teen"
-// console.log(validity)
 
 // task 3 : number compare
 
 let x = 20
 let y = 30
-let compare = (x > y) ? "X is not greater than Y" : "Y is not greater than X"
-// console.log(compare)
+let compare = (x > y) ? "X is greater than Y" : "Y is greater than X"
 
 // task 4 : checking odd
-let num = 21
-let check = (num % 2 === 0) ? 'The number is Odd' : 'The number is Even'
-console.log(check)
+let num = 20
+let check = (num % 2 === 0) ? 'The number is Even' : 'The number is Odd'
 
+// task 5 : finding the biggest number 
 
-// mini challenge : grade system 
+let p = 2
+let q = 5
+let r = 10
+
+if ((p > q) && (p > r)) {
+    console.log("P is the biggest number")
+} else if ((q > p) && (q > r)) {
+    console.log("Q is the biggest number")
+} else {
+    console.log("R is the biggest number")
+}
+
+// mini challenge-1 : grade system 
 
 let mark = 59
 
@@ -34,3 +43,9 @@ if (mark >= 80) {
 } else {
     console.log("Better luck next time, you got failed")
 }
+
+// mini challenge-2 : password checker
+
+let password = "1234666"
+let valid = (password.length >= 6) ? "Valid" : "Too Short"
+console.log(valid)
